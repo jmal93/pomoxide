117c98fc17a0883b6414276dd3512d52a11df960
João Marcello <marcello.jao1611@gmail.com>
2025-05-21 19:02:26 -0300

    função para converter o tempo do argumento e testes unitários

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,5 @@
 mod parser;
-use clap::Parser;
-
 
 fn main() {
 }
 
-fn convert_time(time: String) -> Result<u64, String> {
-    let mut total_seconds = 0;
-    let mut current_number = 0;
-
-    for c in time.chars() {
-        if c.is_ascii_digit() {
-            current_number = current_number * 10 + c.to_digit(10).unwrap() as u64;
-        } else if c == 'm' {
-            total_seconds += 60 * current_number;
-            current_number = 0;
-        } else if c == 's' {
-            total_seconds += current_number;
-            current_number = 0;
-        } else {
-            return Err(format!("Caractere inválido: '{}'\n", c));
-        }
-    }
-
-    Ok(total_seconds + current_number)
-}
--- src/parser.rs
+++ src/parser.rs
@@ -1,24 +1,48 @@
-use std::u64;
-
-pub fn convert_time(s: &str) -> Result<u64, String> {
-    let mut total_seconds = 0;
+pub fn convert_time(s: &str) -> Result<u64, &str> {
+    let mut total_seconds: u64 = 0;
     let mut current_value = String::new();
+    let mut has_minutes: bool = false;
+    let mut has_seconds: bool = false;
+    let mut last_digit: char = ' ';
+
     for c in s.chars() {
         match c {
             '0'..'9' => current_value.push(c),
             'm' => {
+                if current_value.is_empty() {
+                    return Err("Value missing for minutes")
+                }
                 total_seconds += current_value.parse::<u64>().unwrap() * 60;
+                if last_digit == 's' {
+                    return Err("Wrong format, minutes must come before seconds");
+                }
+                last_digit = c;
+                has_minutes = true;
                 current_value.clear();
             }
             's' => {
-                total_seconds += current_value.parse::<u64>().unwrap();
+                if current_value.is_empty() {
+                    return Err("Value missing for seconds")
+                }
+                let seconds = current_value.parse::<u64>().unwrap();
+                if seconds >= 60 {
+                    return Err("Max value for seconds must be 59");
+                }
+                total_seconds += seconds;
+                last_digit = c;
+                has_seconds = true;
                 current_value.clear();
             }
-            _ => return Err("erro".to_string()),
+            _ => return Err("Character not valid"),
         };
+    }
+
+    if !current_value.is_empty() && !has_seconds && !has_minutes {
+        return Err("Timestamps not specified");
     }
     Ok(total_seconds)
 }
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -28,6 +52,24 @@ mod tests {
         let test_cases = vec![("10m", 600), ("10s", 10), ("10m10s", 610)];
         for (input, expected) in test_cases {
             assert_eq!(convert_time(input), Ok(expected));
+        }
+    }
+
+    #[test]
+    fn convert_time_fail() {
+        let test_cases = vec![
+            ("a", "Character not valid"),
+            ("10", "Timestamps not specified"),
+            ("10s10m", "Wrong format, minutes must come before seconds"),
+            ("61s", "Max value for seconds must be 59"),
+            ("10m61s", "Max value for seconds must be 59"),
+            ("m", "Value missing for minutes"),
+            ("m10s", "Value missing for minutes"),
+            ("s", "Value missing for seconds"),
+            ("10ms", "Value missing for seconds"),
+        ];
+        for (input, expected) in test_cases {
+            assert_eq!(convert_time(input), Err(expected));
         }
     }
 }
